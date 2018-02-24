27d2bfe31cdfe66cfda81bdf06f819e5b7a34646
Unknown <oms1226@gmail.com>
2018-02-24 23:11:10 +0900

    patch

--- toolbox/win_batch/trtc_compareAndOverwrite.bat
+++ toolbox/win_batch/trtc_compareAndOverwrite.bat
@@ -60,6 +60,35 @@ rem    fc /B !AFILE! !BFILE!
     )
 )
 
+set WORK_FOLDER=C:\_webRTC\trtc\third_party
+set TARG_FOLDER=Z:\trtc_ing\trtc\third_party
+
+cd %WORK_FOLDER%
+rem FOR /F "delims=" %%A in ('dir /OD /B') DO (
+FOR /F "delims=" %%A in ('dir /S /B') DO (
+    rem echo "%%~fA"
+    set AFILE="%%~fA"
+    set BFILE=!AFILE:%WORK_FOLDER%=%TARG_FOLDER%!
+    if exist !AFILE!\ (
+      rem echo "!AFILE! is a folder."
+    ) else (
+    if exist !BFILE! (
+rem    fc /B !AFILE! !BFILE!
+    fc !AFILE! !BFILE! > nul
+    IF !errorlevel! EQU 1 (
+        rem echo "errorlevel:!errorlevel!"
+        rem echo "!AFILE! is different to !BFILE!"
+        xcopy /Y !AFILE! !BFILE!
+        echo "!AFILE! is copied to !BFILE!"
+    ) else (
+      rem echo "!BFILE! is not exist."
+    )
+    )
+    )
+)
+
+
+
 IF "%time:~0,1%" == " " (
 set END__TIME=%date:~0,4%%date:~5,2%%date:~8,2%0%time:~1,1%%time:~3,2%%time:~6,2%
 ) ELSE (
